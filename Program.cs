6119b4ecfe5559628a2bc31385293d88b4a21e38
ggautterr <ggautterr@gmail.com>
2024-06-26 16:07:50 +0500

    Add Calculation

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace Calculator
     {
         static public void Main()
         {
-            
+            // const float pi = 3.14f;
+
+            Console.Write("First number: ");
+            string firstUserValue = Console.ReadLine();
+
+            Console.Write("Second number: ");
+            string secondUserValue = Console.ReadLine();
+
+            decimal firstNumber = Convert.ToDecimal(firstUserValue);
+            decimal secondNumber = Convert.ToDecimal(secondUserValue);
+
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+            Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+            Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+            Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+            Console.WriteLine($"{firstNumber} % {secondNumber} = {firstNumber % secondNumber}");
         }
     }
 }
